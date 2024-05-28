55c1e80d2d59d5814bfd556aa1f354d0a63d7f83
unknown <garvit31602@gmail.com>
2024-05-28 23:48:47 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './App.css'
 
 function App() {
+  const [pokemonData, setPokemonData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon');
+        const data = await response.json();
+        setPokemonData(data.results);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredPokemon = pokemonData.filter((pokemon) =>
+    pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='top'>
+      <div className='heading'>Pokemon Search</div>
+      <input
+        type="text"
+        placeholder="Search Pokemon"
+        value={searchTerm}
+        onChange={handleSearch}
+      />
+      </div>
+      <div className="pokemon-container">
+        {filteredPokemon.map((pokemon, index) => (
+          <div key={index} className="pokemon-card">
+            <img
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+                index + 1
+              }.png`}
+              alt={pokemon.name}
+            />
+            <p>{pokemon.name}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
