158a3c88f53f20e8421eaadd9ea107e601939d3a
unknown <garvit31602@gmail.com>
2024-11-05 00:29:18 +0530

    ff

--- src/Signup.js
+++ src/Signup.js
@@ -20,7 +20,7 @@ export default function Signup() {
         return;
     }
     try {
-      const response = await fetch("https://backend-alpha-puce-22.vercel.app/api/signup", {
+      const response = await fetch("http://localhost:3000/api", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password })
